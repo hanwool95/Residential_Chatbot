3334c1e703c7fc9e3ae5f4f35dec9796a59af380
hanwool95 <77750865+hanwool95@users.noreply.github.com>
2022-01-07 15:46:27 +0900

    user promise to fix send message bug

--- websocket_paracitce/WebSocketEchoServer.js
+++ websocket_paracitce/WebSocketEchoServer.js
@@ -5,6 +5,7 @@ let wss = new WebSocketServer( { port: 8100 } );
 
 wss.on( 'connection', function(ws){
  console.log("connected");
+ // socket.js에서 send로 받은 val()를 msg로.
  ws.on( 'message', function(msg){
   console.log("msg[" + msg + "]" );
   ws.send( msg );
--- websocket_paracitce/socket.js
+++ websocket_paracitce/socket.js
@@ -1,13 +1,13 @@
 // ws 변수 선언
 let ws;
 
+// 웹 소켓 선언
+ws = new WebSocket("ws://localhost:8100");
+
 $( document ).ready( function()
   {
       // view의 텍스트 모아두는 div 타겟하여 txtRecv
       let txtRecv = $('#Recv');
-
-      // 웹 소켓 선언
-      ws = new WebSocket("ws://localhost:8100");
 
       // ws의 onopen 함수. 서버 연결되면 동작. e는 event
       ws.onopen = function(e){
@@ -16,13 +16,18 @@ $( document ).ready( function()
 
       // ws의 onmessage 함수. 웹소켓에서 메세지 수신 있을 경우 동작. e는 메세지
       ws.onmessage = function(e){
-          txtRecv.append( e.data + "<br>" );
+          e.data.text().then((s) =>{
+              txtRecv.append( s + "<br>" );
+              console.log(s)
+          })
+          // txtRecv.append( e.data.text() + "<br>" );
+          // console.log(e.data)
    };
       // ws의 onclose 함수. 웹소켓 세션이 종료될 경우 동작. e는 close event
       ws.onclose = function(e){
           txtRecv.append( "closed<br>" );
    }
-  } );
+  });
 
 // 서버로 메세지 전송하는 함수. on Click event 발생시 실행.
 function sendMessage()
@@ -31,6 +36,7 @@ function sendMessage()
       let txtSend = $('#Send');
       // ws에 정보 전송.
       ws.send( txtSend.val() );
+      // $('#Recv').append( txtSend.val() + "<br>")
       // Box 초기화.
       txtSend.val( "" );
   }
