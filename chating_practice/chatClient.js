373f1355e23b4925a6ba29594fd5b92912df226c
hanwool95 <77750865+hanwool95@users.noreply.github.com>
2022-01-12 18:25:18 +0900

    user promise to fix send message bug

---
+++ chating_practice/chatClient.js
@@ -0,0 +1,38 @@
+window.onload = function(){
+    //웹소켓 연결하여 클라이언트 소켓 정의.
+    var socket = io.connect('ws://127.0.0.1:3000');
+    // id를 참조하여 메세지 정의. div는 메세지 박스, txt는 메세지 내용.
+    var div = document.getElementById('message');
+    var txt = document.getElementById('txtChat');
+    //텍스트 박스에 포커스 주기
+    txt.focus();
+
+    // 텍스트박스 element에 onkeydown이라는 이벤트 발생시 실행될 바인딩 함수(고유 함수 제작)
+    txt.onkeydown = sendMessage.bind(this);
+    function sendMessage(event){
+        // event.keyCode 13은 Enter. 즉 Enter event 발생시 동작.
+        if(event.keyCode == 13){
+            // message는 이벤트 발생했을 때 메세지값을 받음.
+            var message = event.target.value;
+            // 메세지 입력되었는지 확인
+            if(message){
+                // 엔터 + 메세지 있으면 소켓서버에 serverReceviver라는 이벤트 활성화하여 메세지 값 전송.
+                socket.emit('serverReceiver', message);
+                //텍스트박스는 초기화
+                txt.value = '';
+            }
+        }
+    };
+
+    //ClientReceiver라는 이벤트 함수를 켜놓는 상황 정의. 서버로부터 이 이벤트 호출 받는다면 실행.
+    // data 안에는 Client가 전송한 clientID 정보, message 정보가 있음.
+    socket.on('clientReceiver', function(data){
+        //채팅창에 출력할 메세지 string을 합성. 서버로부터 받아온 data 안의 정보를 활용하여 제작.
+        var message = '['+ data.clientID + '님의 말' + '] ' + data.message;
+        // div 채팅창에 메세지 추가 이후 띄어쓰기.
+        div.innerText += message + '\r\n';
+        // div 채팅창 스크롤바 내리기
+        div.scrollTop = div.scrollHeight;
+    });
+};
+
---
+++ chating_practice/chatServer.js
@@ -0,0 +1,46 @@
+
+var app = require("express")();
+var url = require("url");
+
+
+// "/"은 route. get()은 주소에 대한 응답 처리 함수 정의.
+app.get("/", function(req, res){
+    console.log("get:chatClient.html");
+    // 서버에 접속하는 client에게 서버에 형성되어 있는 Client용 html 파일 전송
+    res.sendFile("chatClient.html", {root: __dirname});
+});
+
+// route("/") 뒤에 무언가 있을 경우 use 호출. url pathname에서 루트의 /를 제외한 나머지를 바탕으로 파일 검색.
+app.use(function(req, res){
+    var fileName = url.parse(req.url).pathname.replace("/","");
+    res.sendFile(fileName, {root: __dirname});
+    console.log("use:", fileName);
+});
+
+//http 모듈 이용하여 지금까지 정의한 app을 집어 넣어 서버를 생성하고, 서버는 포트 3000으로 계속 listen 상태에 돌입.
+var server = require('http').createServer(app);
+server.listen(3000);
+console.log("listening at http://127.0.0.1:3000...");
+
+
+//클로저를 사용해, private한 유니크 id를 만든다. 즉, 사용자별 고유 id 생성을 위해 별도의 id를 정의하는 함수가 있는데
+// 이 함수는 동작할 때마다 id가 증가.
+var uniqueID = (function(){
+    let id = 0;
+    return function(){ return id++; };
+})();
+
+//socket.io 모듈을 이용하여 서버 소켓 생성 후 정의
+var socket = require('socket.io')(server);
+//소켓 Connection 이벤트가 발생했을 때 동작하는 함수 정의.
+socket.sockets.on('connection', function(client){
+    //private id를 만들기 위한 함수를 동작하는 것으로 그 상황의 ID를 clientID로 정의.
+    var clientID = uniqueID();
+    console.log('Connection: '+ clientID);
+
+    //ServerReceiver라는 이벤트 함수를 켜놓는 상황 정의. 클라이언트에서 이것을 호출하면 서버는 받게 됨.
+    client.on('serverReceiver', function(value){
+    //클라이언트 reciever에 해당 ID와 메세지 전송.
+    socket.sockets.emit('clientReceiver', {clientID: clientID, message: value});
+  });
+});
