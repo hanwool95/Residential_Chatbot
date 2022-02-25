b755f073906f0a63df02542f77bbad93c6464af0
hanwool95 <77750865+hanwool95@users.noreply.github.com>
2022-02-25 18:16:12 +0900

    Fix: erase secrete

--- main_app/bot_api/bot_api/settings.py
+++ main_app/bot_api/bot_api/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8rsv9du--#nv-q0zeafdqu+-g86$hb0h4x7p(!sw^3^0ud0@k0'
+SECRET_KEY = key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
